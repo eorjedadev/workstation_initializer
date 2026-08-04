d5346819381a6b6194f9a9a1e8bea8cf710a6137
eorjedadev <e.orjeda.pro@gmail.com>
2026-08-04 06:40:16 -0500

    Commit v1.0.4

--- model/workstation.py
+++ model/workstation.py
@@ -6,15 +6,15 @@ class WorkInitializer:
 
     def ejecutarBria(self):
         # Ejecutar Bria
-        self.pyautogui.moveTo(432, 147, 1)
+        self.pyautogui.moveTo(432, 147, 5)
         self.pyautogui.click(clicks=2)
 
     def ejecutarEscritorioRemoto(self):
         # Ejecutar Escritorio remoto
-        self.pyautogui.moveTo(336, 149, 3)
+        self.pyautogui.moveTo(336, 149, 7)
         self.pyautogui.click(clicks=2)
         # Ejecutar Portapapel
-        self.pyautogui.moveTo(490, 402, 1)
+        self.pyautogui.moveTo(490, 402, 2)
         self.pyautogui.click()
         # Ejecutar Conectar
         self.pyautogui.moveTo(815, 550, 1)
@@ -22,18 +22,18 @@ class WorkInitializer:
 
     def ejecutarEccloger(self):
         # Ejecutar Ingresar usuario
-        self.pyautogui.moveTo(600, 345, 30)
+        self.pyautogui.moveTo(600, 345, 60)
         self.pyautogui.click()
         self.pyautogui.write('47981033', interval=0.25)
         # Ejecutar Ingresar contraseña
-        self.pyautogui.moveTo(900, 36, 1)
+        self.pyautogui.moveTo(900, 36, 5)
         self.pyautogui.click()
         self.pyautogui.write('*A0108#h1732Sab+', interval=0.25)
         self.pyautogui.press('enter')
         self.pyautogui.press('enter')
         # Iniciar sesión workdesktop
-        self.pyautogui.moveTo(221, 746, 1)
+        self.pyautogui.moveTo(221, 746, 3)
         self.pyautogui.click()
         # minimizar ventana de escritorio remoto
-        self.pyautogui.moveTo(885, 9, 1)
+        self.pyautogui.moveTo(885, 9, 2)
         self.pyautogui.click()
